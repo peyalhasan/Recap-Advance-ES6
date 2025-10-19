387d7ff5b9509b20f7f919a12762c88d8e81fa91
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-19 22:43:00 +0600

    Passby value & pass by Referance

--- referance.js
+++ referance.js
@@ -1,8 +1,21 @@
-
+// Pass by value
+/* Work in Premetive Data type. Like number, boolean , string */
 function changeNumber(num){
     num = num + 10;
-    console.log('Inside Function',num)
+    console.log('Inside Function',num) // Changed but copyed data. 
 }
 const myNumber = 10;
 changeNumber(myNumber);
-console.log('Outside Function', myNumber);
+console.log('Outside Function', myNumber); // Unchanged original
+
+// Pass by  Referance 
+// In non premetive data type it will happen. Like object, array
+
+function changeName(person){
+    person.name = 'Rahim';
+    console.log('Inside Function: ', person.name) // Changed original
+}
+
+const person = {name: 'Karim'};
+changeName(person);
+console.log('Outside Function', person.name) // Already Changed
